e31d35d1c46f5de0759bf97044177f90db52acb3
Alfa <Alfa@KA-B09-103.alfatraining.local>
2018-05-16 15:33:12 +0200

    UpDate

---
+++ TOMProjekte/src/schuleAufgaben052018/AufgabenW3T1d.java
@@ -0,0 +1,92 @@
+package schuleAufgaben052018;
+
+public class AufgabenW3T1d {
+
+	public static void main(String[] args) {
+		// ======  W4T1 - Equals,Comparable, Comparatorn - Aufgaben =====
+/*	Aufgabe 1
+	
+	a) Erklären Sie die Unterschiede zwischen der equals()-Methode und dem "=="-Operator. 
+	   In welchen Fällen wird == true und in welchen equals.
+	b) Wie könnte eine sinnvolle equals-Methode für folgende Klasse in etwa aussehen.
+	   Schreiben Sie den Code auf!
+*/     
+	class Hantel { 
+		int gewicht; 
+		
+		public Hantel(int gewicht) { 
+			this.gewicht=gewicht; 
+  } 
+		public int compareTo(Hantel o) {
+			
+				
+			Name
+			String s1 = new String("text");
+			
+			Hantel[ hanteln = { new Hantel(19), new Hantel(11), new SpecialHantel() };
+			
+			Object[] objekte = { new Hantel(11), new Hantel(11), new Hantel(10) };
+		}
+				
+	}	
+	
+	interface Firma {
+		public float bereite();
+		
+	}
+	
+	class Tisch implements Firma, Comparable {
+		public float bereite() {
+			return 0.1f;
+		}
+		
+		@Override
+		public int compareTo(Object o) {
+			if(o instanceof Tisch) {
+				Tisch t1 = (Tisch)o;
+				if (this.bereite() > this.bereite()) {
+					return -1;
+				}
+			}
+			return 0;
+				}
+			}
+	
+	class Schaf {
+		
+	}
+	
+		// Comparable und Comparator
+	
+	// Sortierklasse mit Comparator:
+	class SortClass implements Comparable<Hantel> {
+
+			@Override
+			public int compare(Hantel h1, h2) {
+				if(h2.gewicht > h1.gewicht) {
+					
+					
+					
+				}
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			int gewicht;
+		}
+		}
+			public Hantel(int gewicht) {
+				this.gewicht = gewicht;
+			}
+		// Methode aus Comparable
+			public int compareTo(Object o) {
+				int ergebniss = o.gewicht-this.gewicht;
+				
+			}
+		}
+	class SpezialHantel extends Hantel {
+		
+	
+	
+	}
+
+
---
+++ TOMProjekte/src/schuleAufgaben052018/AufgabenW3T5.java
@@ -0,0 +1,10 @@
+package schuleAufgaben052018;
+
+public class AufgabenW3T5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
