c6973b0bdba6fdda050349498c6a3efdef874822
Alfa <Alfa@KA-B09-103.alfatraining.local>
2018-05-17 15:35:36 +0200

    UpDate

---
+++ TOMProjekte/src/schuleAufgaben052018/AufgabenW4T4c.java
@@ -0,0 +1,46 @@
+package schuleAufgaben052018;
+
+public class AufgabenW4T4c {
+
+	public static void main(String[] args) {
+		/* Aufgaben W4T4
+		    Aufgabe 1
+		
+		a) Starten Sie Apache und MySQL über das XAMPP Control Panel und rufen Sie die  MySQL Admin-Seite auf ( http://localhost/phpmyadmin/). 
+			
+		b) Erstellen Sie eine neue Datenbank mit dem Namen KundenDBA über das MySQL Admin Tool und legen Sie darin eine Tabelle Kunden an.  
+		   Die Tabelle soll folgende Spalten besitzen:  
+		   KID (INT, AutoIncrement, PrimaryKey), Nachname (Varchar 16), Vorname (Varchar 16), Email (Varchar 30). 
+		   
+		c) Erstellen Sie in Java ein neues Package und eine Klasse DBVerbindung und stellen Sie eine Verbindung zu ihrer neu erzeugten Datenbank her. 
+		   Fügen Sie mit einem Insert-Statement folgende Fälle in ihre Tabelle ein: 
+
+
+		Vorname 	Nachname Email 
+
+		Verena		Vetter 		vetter@email.com
+		
+ 		Paul		Müller		mueller@muellermail.co
+ 		
+		Hildegard	von Bingen	hildeb@bingen.de
+		
+		
+ 		Lara		Rehfeld		larareh@feld.de
+ 		
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+*/		
+		
+		
+		
+	}
+
+}
